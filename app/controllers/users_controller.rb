c878c721ef5814e37f8f7b1a0ec836c2180a67a6
Ben Everard <bfeverard@gmail.com>
2013-06-03 15:20:16 -0700

    Rails Basic 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,18 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@users = User.new
   end
 
   def create
+  	@users = User.new(params[:user])
+    if @users.save
+      redirect_to @users, notice: 'Post was successfully created.'
+    else
+      render action: "new"
+    end
+  end
+  def show
+  	@users = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :email_address, :first_name, :last_name, :password
+  attr_accessible :email_address, :first_name, :last_name, :password, :password_confirmation
+
+  validates :first_name, :last_name, :email_address, :password, :password_confirmation, :presence => true
+  validates :first_name, :length => { :minimum => 2 }
+  validates :last_name, :length => { :minimum => 2 }
+  validates :email_address, :uniqueness => true
+  validates :password, :confirmation => true
 end
+				
